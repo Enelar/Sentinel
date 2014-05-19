c4b50fe6a964b9171607658e63b9fd7c75ea910f
Kirill Berezin <enelar@develop-project.ru>
2014-05-19 07:02:10 +0400

    Getting answer from minecraft servers

--- Sentinel/main.cpp
+++ Sentinel/main.cpp
@@ -4,13 +4,86 @@
 #include <iostream>
 #include <fstream>
 #include <deque>
+#include <memory>
+#include <sstream>
+#include <codecvt>
+
+#define _WIN32_WINDOWS 0x0501
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
 
 
-using namespace std;
+using namespace ::std;                                                                      
+using namespace ::boost::asio::ip;
+using namespace ::boost::asio;
 
-int GetServerOnline(string addr, int port)
+static_assert(sizeof(short) == 2, "Short should be 2 bytes size");
+
+string GetServerAnswer(string addr, int port)
 {
-  return 0;
+  auto TheMessage = async([=]()
+  {
+    stringstream res;
+
+    auto MinecraftString = [](string origin)
+    {
+      stringstream res;
+      res << short(origin.length());
+
+      wstring_convert<codecvt<char16_t, char, mbstate_t>, char16_t> convert;
+      u16string converted = convert.from_bytes(origin);
+
+      // Get bytearray of utf-16 string
+      res << string((char *)converted.c_str());
+
+      return res.str();
+    };
+
+    res << "\xFE\x01\xFA";
+    res << MinecraftString("MC|PingHost");
+
+    auto hostname = MinecraftString(addr);
+
+    res << short(7 + hostname.length());
+    res << char(73);
+    res << hostname;
+    res << int(port);
+    return res.str();
+  });
+
+  boost::system::error_code error;
+  try
+  {
+    static boost::asio::io_service io_service;
+    tcp::iostream stream(addr, boost::lexical_cast<string>(port));
+    stream << TheMessage.get();
+    stream.flush();
+    char kicked;
+    short len;
+    stream >> kicked >> len;
+
+    u16string reason = [len, &stream]()
+    {
+      const int size = len * sizeof(short);
+      string buf;
+      buf.resize(size);
+
+      stream.read(&buf[0], size);
+      u16string ret;
+      ret.resize(len);
+      memcpy_s(&ret[0], size, &buf[0], size);
+      return ret;
+    }();
+
+    wstring_convert<codecvt<char16_t, char, mbstate_t>, char16_t> convert;
+    return convert.to_bytes(reason);
+  }
+  catch (exception &e)
+  {
+    return e.what();
+  }
 }
 
 struct abort_execution
@@ -25,15 +98,14 @@ void Worker( const string &filename )
   if (!file.is_open())
     throw abort_execution();
 
-  deque<future<int>> tasks;
-  packaged_task<int(string, int)> task(GetServerOnline);
+  deque<future<string>> tasks;
 
   while (!file.eof())
   {
     string address;
     int port;
 
-    tasks.push_back(async(std::launch::async, GetServerOnline, address, port));
+    tasks.push_back(async(std::launch::async, GetServerAnswer, address, port));
   }
 
   for (auto &result : tasks)
